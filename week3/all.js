104adb34d0a6dd541680ff5738e0a5f3b30021b8
dengmeidada <k333441@gmail.com>
2020-07-04 10:48:56 +0800

    week3

---
+++ week3/all.js
@@ -0,0 +1,21 @@
+new Vue({
+    el:'#app',
+    data:{
+        products:[
+            {
+                title:'商品名稱',
+                category:'商品分類',
+                content:'商品描述',
+                description:'商品說明',
+                imageUrl:'商品圖片',
+                enabled:false,
+                origin_price:'原價',
+                price:'售價',
+                unit:'單位'
+            }
+        ]
+    },
+    methods: {
+        
+    },
+})
